612c2e4b1a1480df0fbdbe6692a22a9bfe5cee05
Keita Watanabe <keitaw09@gmail.com>
2020-02-17 08:38:06 +0900

    Check path function

--- my_app.py
+++ my_app.py
@@ -7,10 +7,12 @@ def show_config(cfg):
     print(cfg.pretty())
 
 @hydra.main()
-def my_app(cfg: DictConfig) -> None:
+def check_path(cfg: DictConfig) -> None:
     print(f'Current working directory: {os.getcwd()}')
     print(f'Orig working directory : {hydra.utils.get_original_cwd()}')
     print(f'to_absolute_path("foo") : {hydra.utils.to_absolute_path("foo")}')
     print(f'to_absolute_path("/foo") : {hydra.utils.to_absolute_path("/foo")}')
+
 if __name__ == "__main__":
     show_config()
+    check_path(config_path='conf/config.yaml')
