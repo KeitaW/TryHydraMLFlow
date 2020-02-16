226765fa738fafe520e0f6f2c928d8a20297c029
Keita Watanabe <keitaw09@gmail.com>
2020-02-17 08:37:22 +0900

    Refactor function name

--- my_app.py
+++ my_app.py
@@ -1,8 +1,16 @@
+import os
+from omegaconf import DictConfig
 import hydra
 
 @hydra.main(config_path='conf/config.yaml')
-def my_app(cfg):
+def show_config(cfg):
     print(cfg.pretty())
 
+@hydra.main()
+def my_app(cfg: DictConfig) -> None:
+    print(f'Current working directory: {os.getcwd()}')
+    print(f'Orig working directory : {hydra.utils.get_original_cwd()}')
+    print(f'to_absolute_path("foo") : {hydra.utils.to_absolute_path("foo")}')
+    print(f'to_absolute_path("/foo") : {hydra.utils.to_absolute_path("/foo")}')
 if __name__ == "__main__":
-    my_app()
+    show_config()
